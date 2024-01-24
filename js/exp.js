411b575f2d303d19c308c58a461b3654c236623f
David E Melnikoff <davidemelnikoff@gmail.com>
2024-01-23 16:59:23 -0800

    Update exp.js

--- js/exp.js
+++ js/exp.js
@@ -28,7 +28,7 @@ const exp = (function() {
     let settings = {
         nSpins: 5,
         effortOrder: jsPsych.randomization.repeat(['highEffort', 'lowEffort'], 1),
-        miOrder: [['highMI', 'lowMI'], ['lowMI', 'highMI']][miDraw];
+        miOrder: [['highMI', 'lowMI'], ['lowMI', 'highMI']][miDraw],
         numOutcomes: [['8', '2'], ['2', '8']][miDraw];
         lowMI_wheel: [lowMI_wheel1, lowMI_wheel1][wheelDraw],
         highMI_wheel: highMI_wheel,
